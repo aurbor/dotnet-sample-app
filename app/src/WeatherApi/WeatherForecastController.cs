32d923477e466f725d3153e9e1cb57ba7aa4e43f
aurbor <evan@aurbor.com>
2025-09-04 13:32:07 +0100

    Initial Commit

---
+++ app/src/WeatherApi/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
+    };
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast(DateTime.Now.AddDays(index), Random.Shared.Next(-20, 35), Summaries[Random.Shared.Next(Summaries.Length)])
+        );
+    }
+}
